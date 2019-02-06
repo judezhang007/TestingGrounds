2e7459b3cb249bfca75e6433de0a4a68f5d5d533
szhang <szhang@guidewire.com>
2019-02-06 13:14:13 +0800

    Using C++ for behavior tree instead of BP.

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -5,6 +5,20 @@
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("This is a test!!!"));
+	//Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	//Set next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	//Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
 	return EBTNodeResult::Succeeded;
 }
--- Source/TestingGrounds/Public/ChooseNextWaypoint.h
+++ Source/TestingGrounds/Public/ChooseNextWaypoint.h
@@ -5,6 +5,9 @@
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTTaskNode.h"
 #include "BehaviorTree/BehaviorTreeTypes.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" //todo: reomve coupling
 #include "ChooseNextWaypoint.generated.h"
 
 /**
@@ -16,4 +19,11 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 	
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 };
